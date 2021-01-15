d1453cc591dfce4193aa62e2853912f307e4384b
stnlykwk <36062904+stnlykwk@users.noreply.github.com>
2021-01-14 18:06:47 -0800

    Remove credentials

--- send_sms.py
+++ send_sms.py
@@ -4,8 +4,8 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'AC2854bff1bbfbbdb441498605145b3a93'
-auth_token = 'a959f9fb939474f8354bbd0a922c131e'
+account_sid = ''
+auth_token = ''
 client = Client(account_sid, auth_token)
 
 message = client.messages \
